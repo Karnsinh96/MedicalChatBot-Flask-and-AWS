f670a96280e67fa20f83d9c0f2986d7a4ff4351b
Kilo Charlie <karnsinh96@gmail.com>
2026-04-29 16:51:21 +0530

    Requiments Installed Successfully

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Kilo Charlie",
+    author_email="karnsinh96@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
